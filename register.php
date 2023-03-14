6bf16efb048a9f49cedabd3c74bec730e41f8304
Alexander Chea <58100801+cheaseeds@users.noreply.github.com>
2023-03-14 08:54:09 -0700

    Added error message output and registration logic

--- register.php
+++ register.php
@@ -18,30 +18,34 @@ if (!$conn) {
 }
 
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-    $username = $_POST['username'];
-    $password = $_POST['password'];
-    $first_name = $_POST['first_name'];
-    $last_name = $_POST['last_name'];
-    $email_address = $_POST['email_address'];
+    $username = !empty($_POST['username']) ? $_POST['username'] : null;
+    $password = !empty($_POST['password']) ? $_POST['password'] : null;
+    $first_name = !empty($_POST['first_name']) ? $_POST['first_name'] : null;
+    $last_name = !empty($_POST['last_name']) ? $_POST['last_name'] : null;
+    $email_address = !empty($_POST['email_address']) ? $_POST['email_address'] : null;
 
-    $result = mysqli_query($conn, "SELECT * FROM users WHERE uname = '$username'");
-    if (mysqli_num_rows($result) == 1) {
-        $error = "Username already exists!";
-        header("Location: register.php?error=$error"); 
-        exit();
+    if (!$username || !$password || !$first_name || !$last_name || !$email_address) {
+        $error = "All fields are required";
     } else {
+        $result = mysqli_query($conn, "SELECT * FROM users WHERE uname = '$username'");
+        if (mysqli_num_rows($result) == 1) {
+            $error = "Username already exists!";
+            header("Location: register.php?error=$error"); 
+            exit();
+        } else {
 
-        $password = password_hash($password, PASSWORD_DEFAULT);
-        $sql = "INSERT INTO users (uname, pword, user_first_name, user_last_name, email_address) VALUES ('$username', '$password', '$first_name', '$last_name', '$email_address')";
+            $password = password_hash($password, PASSWORD_DEFAULT);
+            $sql = "INSERT INTO users (uname, pword, user_first_name, user_last_name, email_address) VALUES ('$username', '$password', '$first_name', '$last_name', '$email_address')";
 
-    }
-    if (mysqli_query($conn, $sql)) {
-        header("Location: login.php"); 
-        exit();
-    } else {
-        $error = "Error: " . mysqli_error($conn);
-        header("Location: register.php?error=$error"); 
-        exit();
+        }
+        if (mysqli_query($conn, $sql)) {
+            header("Location: login.php"); 
+            exit();
+        } else {
+            $error = "Error: " . mysqli_error($conn);
+            header("Location: register.php?error=$error"); 
+            exit();
+        }
     }
 }
 
@@ -59,10 +63,13 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
         </h1>
     </header>
     <form action="register.php" method="post">
-
+        
         <h2>Register</h2>
         <?php if (isset($_GET['error'])) { ?>
-            <p class="error"><?php echo $_GET['error']; ?></p>
+            <p><?php echo $_GET['error']; ?></p>
+        <?php } ?>
+        <?php if (!empty($error)) { ?>
+            <p><?php echo $error; ?></p>
         <?php } ?>
         <label>User Name</label>
         <input type="text" name="username" placeholder="User Name"><br>
@@ -75,7 +82,9 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
         <label>Email Address</label>
         <input type="text" name="email_address" placeholder="Email Address"><br>  
         <button type="submit">Register</button>
-
+        <a onclick="location.href='login.php'">Back to Login</a>
+        
     </form>
+    
 
 </html>
